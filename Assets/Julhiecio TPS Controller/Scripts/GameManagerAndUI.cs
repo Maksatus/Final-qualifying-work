4d993b1b2f3f23d267587fb24020144e852c66c6
Maksim Tretyakov <m.tretyakov@kefirgames.com>
2022-05-27 23:08:25 +0300

    init new repos

---
+++ Assets/Julhiecio TPS Controller/Scripts/GameManagerAndUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class GameManagerAndUI : MonoBehaviour
+{
+	[Header("UI Settings")]
+	public bool showInteractText;
+	public Text interactText;
+	
+	void Update()
+	{
+		interactText.text = showInteractText ? "Нажмите [F] для подробной информации" : "";
+	}
+}
